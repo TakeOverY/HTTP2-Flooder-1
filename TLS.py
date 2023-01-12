5a247d3390b89c8a1fd340b1f1777ae05193c498
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-01-12 16:49:49 +0700

    Update TLS.py

--- TLS.py
+++ TLS.py
@@ -1,5 +1,6 @@
 import os
 from os import name, system
+from pystyle import Colors, Colorate, Center
 if name == 'nt':
     system("title Đậu Đậu - HTTP2 Tunnel")
     system("mode 101, 25")
@@ -15,19 +16,21 @@ panel = """
     $$ |   $$       |$$    $$/           $$  $$/ $$    $$/ $$ |  $$ |$$ |  $$ |$$       |$$ |      
     $$/    $$$$$$$$/  $$$$$$/             $$$$/   $$$$$$/  $$/   $$/ $$/   $$/  $$$$$$$/ $$/       
 """
-print(panel)
-host=input("       [+] Nhập URL Taget: ")
-time=int(input("       [+] Nhập Time (s): "))
-print("  =========================================================================")
-input('       Vui lòng xác nhận attack (Enter)')
+print(Colorate.Diagonal(Colors.red_to_yellow, Center.XCenter(panel)))
+print()
+host=input(Colorate.Diagonal(Colors.red_to_yellow, "       [+] Nhập URL Taget: "))
+time=int(input(Colorate.Diagonal(Colors.red_to_yellow, "       [+] Nhập Time (s): ")))
+print(Colorate.Diagonal(Colors.red_to_yellow, "  ========================================================================="))
+input(Colorate.Diagonal(Colors.red_to_yellow, '       Vui lòng xác nhận attack (Enter)'))
 os.system("cls" if os.name == "nt" else "clear")
 print()
-print("  =========================================================================")
-print("       Mục tiêu               =     {}".format(host))
-print("       Thời gian              =     {} giây".format(time))
-print("       Methoads (default)     =     GET")
-print("       Cổng (default)         =     443")
-print("  =========================================================================")
+print(Colorate.Diagonal(Colors.red_to_yellow, "  ========================================================================="))
+print(Colorate.Diagonal(Colors.red_to_yellow, "       Mục tiêu               =     {}".format(host)))
+print(Colorate.Diagonal(Colors.red_to_yellow, "       Thời gian              =     {} giây".format(time)))
+print(Colorate.Diagonal(Colors.red_to_yellow, "       Methoads (default)     =     GET"))
+print(Colorate.Diagonal(Colors.red_to_yellow, "       Cổng (default)         =     443"))
+print(Colorate.Diagonal(Colors.red_to_yellow, "  ========================================================================="))
 os.system(f"node TLS.js {host} {time}")
-print("  =========================================================================")
-input('       ấn Enter để thoát')
+print(Colorate.Diagonal(Colors.red_to_yellow, "  ========================================================================="))
+input(Colorate.Diagonal(Colors.red_to_yellow, '       ấn Enter để thoát'))
+# https://github.com/DauDau432/HTTP2-Flooder/
