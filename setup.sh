3500e4728097d32354916ca1df42e629c89de331
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-04-07 22:44:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ yum install python3-pip -y
 pip3 install pystyle
 apt install npm -y
 yum install npm -y
-bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/scan/main/vps)
+#bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/scan/main/vps)
 cd /root/HTTP2-Flooder
 npm i http http2 crypto tls
 clear
